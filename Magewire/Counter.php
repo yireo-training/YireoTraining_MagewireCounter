d6ece874106f059860641fe1fd892b925f26a6de
Jisse Reitsma <jisse@yireo.com>
2023-10-27 08:45:15 +0200

    Refactor

--- Magewire/Counter.php
+++ Magewire/Counter.php
@@ -2,19 +2,33 @@
 
 namespace YireoTraining\MagewireCounter\Magewire;
 
+use Magento\Customer\Model\Session;
 use Magewirephp\Magewire\Component;
 
 class Counter extends Component
 {
+    public function __construct(
+        private Session $session
+    ) {
+    }
+
     public int $counter = 0;
+
+    public function mount(): void
+    {
+        $this->counter = (int)$this->session->getData('counter');
+    }
 
     public function increment()
     {
         $this->counter++;
+        $this->session->setData('counter', $this->counter);
+        $this->dispatchSuccessMessage(__('Counter is now '.$this->counter));
     }
 
     public function decrement()
     {
         $this->counter--;
+        $this->session->setData('counter', $this->counter);
     }
 }
